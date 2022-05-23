82c6dc1caf3883afb6d3ddbdf485b8664e229d45
danzou1ge6 <101497246+danzou1ge6@users.noreply.github.com>
2022-05-23 17:20:07 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -11,10 +11,10 @@ setup(
         'token_getter': 'dist_stuff/token_getter'
     },
     install_requires=[
-        'click',
-        'aiohttp',
-        'fuzzywuzzy[accelerate_fuzzywuzzy]',
-        'toml'
+        'click==8.1.3',
+        'aiohttp==3.8.1',
+        'fuzzywuzzy[accelerate_fuzzywuzzy]==0.18.0',
+        'toml==2.0.1'
     ],
     extras_require={
         'accelerate_fuzzywuzzy': ['python-levenshtein']
