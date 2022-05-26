27592bab15ebd23d029db517a4e7a60dbf21aa3a
danzou1ge6 <yu_liukun@outlook.com>
2022-05-26 12:34:17 +0800

    fix: 取消/收藏插画后同时修改插画缓存池中的状态

--- wahu_backend/aiopixivpy/api_account_control.py
+++ wahu_backend/aiopixivpy/api_account_control.py
@@ -19,7 +19,7 @@ class AccountControlPixivAPI(BasePixivAPI):
         如果成功，pixiv 服务器会返回一个空 JSON 字符串 `{}`；
         否则会返回 404 错误，以及一个
             {'error': {
-                  'user_message': '不正なリクエストです。', 
+                  'user_message': '不正なリクエストです。',
                   'message': '',
                   'reason': '',
                   'user_message_details': {}
@@ -33,14 +33,18 @@ class AccountControlPixivAPI(BasePixivAPI):
         }
         if tags is not None:
             data['tags[]'] = ' '.join(str(tag) for tag in tags)
-        
+
         self.log_adapter.info(
             'illust_bookmark_add: 尝试添加插画收藏 %s %s'
             % (iid, restrict)
         )
 
         await self.post_json('v2/illust/bookmark/add', data=data)
-    
+
+        # 处理插画详情池
+        if iid in self.ilst_pool.keys():
+            self.ilst_pool[iid].is_bookmarked = True
+
     @check_login
     async def illust_bookmark_delete(self, iid: int) -> None:
         """删除收藏插画"""
@@ -51,6 +55,10 @@ class AccountControlPixivAPI(BasePixivAPI):
         await self.post_json(
             'v1/illust/bookmark/delete', data={'illust_id': iid}
         )
+
+        # 处理插画详情池
+        if iid in self.ilst_pool.keys():
+            self.ilst_pool[iid].is_bookmarked = False
 
     @check_login
     async def user_follow_add(
@@ -67,7 +75,7 @@ class AccountControlPixivAPI(BasePixivAPI):
             'v1/user/follow/add',
             data={'user_id': uid, 'restrict': restrict}
         )
-    
+
     @check_login
     async def user_follow_delete(self, uid: int) -> None:
         """取消关注用户"""
