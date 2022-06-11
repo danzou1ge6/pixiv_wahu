bafed99147328494df7f014cea5fde181953b744
danzou1ge6 <yu_liukun@outlook.com>
2022-06-11 21:53:28 +0800

    feat[cli]: 清空缓存池

--- wahu_cli/pool_status.py
+++ wahu_cli/pool_status.py
@@ -5,7 +5,7 @@ import click
 if TYPE_CHECKING:
     from wahu_backend.wahu_core import CliClickCtxObj
 
-from wahu_backend.wahu_core.wahu_cli_helper import less, wahu_cli_wrap
+from wahu_backend.wahu_core.wahu_cli_helper import less, wahu_cli_wrap, print_help
 
 from helpers import table_factory
 
@@ -15,13 +15,20 @@ DESCRIPTION = '插画缓存池、图片缓存池、生成器缓存池状态'
 
 def mount(wexe: click.Group):
 
-    @wexe.command()
+    @wexe.group()
+    @click.option('--help', is_flag=True, callback=print_help,
+                  expose_value=False, is_eager=True)
+    def pool():
+        """管理缓存池"""
+        pass
+
+    @pool.command()
     @click.option(
         '--verbose', '-v', type=click.Choice(['ilst', 'img', 'gen']),
         help='打印所有键'
     )
     @wahu_cli_wrap
-    async def pool_status(
+    async def status(
         cctx: click.Context, verbose: Optional[Literal['ilst', 'img', 'gen']]
     ):
         """打印缓存池的状态
@@ -38,13 +45,13 @@ def mount(wexe: click.Group):
             tbl.add_rows([
                 ('插画缓存池',
                 f'{len(wctx.papi.ilst_pool)} / {wctx.papi.ilst_pool_size}',
-                f'{len(wctx.papi.ilst_pool) / wctx.papi.ilst_pool_size:.3f}%'),
+                f'{len(wctx.papi.ilst_pool) * 100 / wctx.papi.ilst_pool_size:.3f}%'),
                 ('图片缓存池',
                 f'{len(wctx.image_pool.pool)} / {wctx.image_pool.size}',
-                f'{len(wctx.image_pool.pool) / wctx.image_pool.size:.3f}%'),
+                f'{len(wctx.image_pool.pool) * 100 / wctx.image_pool.size:.3f}%'),
                 ('生成器缓存池',
                 f'{len(wctx.agenerator_pool.pool)} / {wctx.agenerator_pool.size}',
-                f'{len(wctx.agenerator_pool.pool) / wctx.agenerator_pool.size:.3f}%')
+                f'{len(wctx.agenerator_pool.pool) * 100 / wctx.agenerator_pool.size:.3f}%')
             ])
 
             obj.pipe.putline(tbl.get_string())
@@ -58,3 +65,20 @@ def mount(wexe: click.Group):
                 keys = list(wctx.agenerator_pool.pool.keys())
 
             await less('\n'.join(keys), obj.pipe, in_terminal=wctx.in_terminal)
+
+    @pool.command()
+    @click.argument('name', type=click.Choice(['img', 'ilst', 'gen']))
+    @wahu_cli_wrap
+    async def clean(cctx: click.Context, name: Literal['ilst', 'img', 'gen']):
+        """清空缓存池"""
+
+        obj: CliClickCtxObj = cctx.obj
+        wctx = obj.wctx
+
+        if name == 'ilst':
+            wctx.papi.ilst_pool.clear()
+        elif name == 'img':
+            wctx.image_pool.pool.clear()
+        elif name == 'gen':
+            wctx.agenerator_pool.pool.clear()
+
